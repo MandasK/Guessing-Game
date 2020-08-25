a85765772437ad76f9bcd72aaaf13837eb734f3c
MandasK <king.amanda.r@gmail.com>
2020-08-25 10:27:29 -0500

    phase two done

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -9,8 +9,18 @@ namespace GuessingGame
         {
             Console.WriteLine("I'm thinking of a number between 1-100. Guess what the number is correctly and get a PRIZE!");
             Console.Write("What's your guess?");
-            string GuessedNumber = Console.ReadLine();
-            Console.WriteLine(GuessedNumber);
+            string readGuess = Console.ReadLine();
+            int GuessedNumber = int.Parse(readGuess);
+
+            int secretNumber = 42;
+            if (GuessedNumber == secretNumber)
+            {
+                Console.WriteLine("You guessed right and you know the secret to life!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed wrong. BOOOOO!");
+            };
         }
     }
 }
